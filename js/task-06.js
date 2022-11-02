d6a8c7f258a1bd7aac2371722ab3d60688672517
vitsemashko <107743238+vitsemashko@users.noreply.github.com>
2022-11-02 11:59:55 +0200

    Update task-06.js
    Removed extra code line

--- js/task-06.js
+++ js/task-06.js
@@ -1,5 +1,4 @@
 const textInput = document.querySelector("#validation-input");
-console.log(textInput.getAttribute("data-length"));
 textInput.addEventListener("blur", (event) => {
 	if (event.target.value.length == textInput.getAttribute("data-length")) {
 		textInput.classList.add("valid");
