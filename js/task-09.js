35e0d8123a809f0289f11789539d54c4c6f97c64
vitsemashko <107743238+vitsemashko@users.noreply.github.com>
2022-11-03 10:45:40 +0200

    Update task-09.js
    Refactor of task 9 code (function has been devided to two functions, added obj refs)

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,18 @@
-const changeButton = document.querySelector("button.change-color");
-const body = document.querySelector("body");
-const colorValue = document.querySelector("span.color");
+const refs = {
+	changeButton: document.querySelector("button.change-color"),
+	body: document.querySelector("body"),
+	colorValue: document.querySelector("span.color"),
+};
+refs.changeButton.addEventListener("click", addColorToBodyAndSpan);
 
-changeButton.addEventListener("click", getRandomHexColor);
 function getRandomHexColor() {
-	let randomColor = `#${Math.floor(Math.random() * 16777215)
+	return `#${Math.floor(Math.random() * 16777215)
 		.toString(16)
 		.padStart(6, 0)}`;
-	body.setAttribute("style", `background-color: ${randomColor}`);
-	colorValue.textContent = randomColor;
+}
+
+function addColorToBodyAndSpan() {
+	let randomColor = getRandomHexColor();
+	refs.body.setAttribute("style", `background-color: ${randomColor}`);
+	refs.colorValue.textContent = randomColor;
 }
